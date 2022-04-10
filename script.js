d49e224552627a60db6fa2b2ca7b2451a4fcbb69
AhmedAbdulwasi <98428365+AhmedAbdulwasi@users.noreply.github.com>
2022-04-10 17:25:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var quotes = [
 'Success isn\'t guaranteed. Hard work doesn\'t guarantee success. You can work as hard as you want It doesn\'t mean you will get success. Drive gives success. You can work hard without having the drive too.',
 'You got the Canadian mindset Bro I\'m more foreign than you. Cause You think rich I think poor. But I have the mentality For the grind to be rich. Which is superior to your way of thinking.',
 'Y\'all just looking for a false future',
+'My enemies keep asking why I pray for them to go to jail instead of praying for their death and I say the exact same answer to them. I want them to be alive to witness their downfall.',
 'So you want your enemy and rival to witness your progression? That gives them the drive to be better than you. Make them think you dumb and weak. Show up when needed.'
 ]
 
