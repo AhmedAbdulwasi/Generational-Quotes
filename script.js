2dc6ee05f7e2d1905f7c6f7b1db1175295d3023d
AhmedAbdulwasi <98428365+AhmedAbdulwasi@users.noreply.github.com>
2022-04-30 20:52:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var quotes = [
 
 'Don\'t just listen to my preachings, act on them, reflect on them, my preachings aren\'t meant to touch your ears, they are meant to touch you inside here.',
 'Drip doesn\'t change people, it\'s the will the drip has to change you.',
-'You can\'t keep blaming other people outside the community for the problems in the Muslim community, this problem is internal not external, we gotta address this stuff, if we don\'t then progression will just continue to slow down. These younger generation of Muslims are setting us 15 years back but no one is talking about Be better than that.',
+'You can\'t keep blaming other people outside the community for the problems in the Muslim community. This problem is internal not external. We gotta address this stuff, if we don\'t then progression will just continue to slow down. These younger generation of Muslims are setting us 15 years back but no one is talking about. Be better than that.',
 'Success isn\'t guaranteed. Hard work doesn\'t guarantee success. You can work as hard as you want It doesn\'t mean you will get success. Drive gives success. You can work hard without having the drive too.',
-'You got the Canadian mindset Bro I\'m more foreign than you. Cause You think rich I think poor. But I have the mentality For the grind to be rich. Which is superior to your way of thinking.',
+'You got the Canadian mindset Bro. I\'m more foreign than you. Cause You think rich I think poor. But I have the mentality For the grind to be rich. Which is superior to your way of thinking.',
 'Y\'all just looking for a false future.',
 'Politicians act like they are working for the people and that they are against the upper class (the rich). Ok, why are they millionaires? Think before you support.',
 'Haters be waking up REAL EARLY to have extra time to be a hater.',
